508f69c0b1e1fa43f3de3fdc24567487b7746086
felix <felix0808@yandex.ru>
2023-07-01 16:44:21 +0400

    fix build, add CssInJs

--- hostApp/client/main/App.tsx
+++ hostApp/client/main/App.tsx
@@ -1,9 +1,16 @@
+import { css } from '@emotion/react';
+
 export const App = () => {
   console.log(32);
 
   return (
     <div className="container py-4 bg-white h-full shadow-inner">
       <div className="text-amber-700 text-x1.5">Hi bro!</div>
+      <div css={myClass}>Is styled components ok?</div>
     </div>
   );
 };
+
+const myClass = css`
+  color: green;
+`;
--- hostApp/webpack.config.js
+++ hostApp/webpack.config.js
@@ -1,8 +1,10 @@
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const dirname = url => fileURLToPath(path.dirname(url));
 const __dirname = dirname(import.meta.url);
@@ -14,17 +16,23 @@ const isAnalyze = process.env.ANALYZE;
 
 let config = {
   mode,
+
   devtool: isDev && 'eval-cheap-module-source-map',
+
   entry: path.resolve(__dirname, './client/main/index.tsx'),
+
   output: {
     filename: isProd ? 'js/index.[contenthash:6].js' : 'js/index.js',
     path: path.resolve(__dirname, 'dist/public'),
   },
+
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     extensionAlias: { '.js': ['.ts', '.js'], '.jsx': ['.tsx'] },
   },
+
   stats: { warnings: false, children: false, modules: false },
+
   module: {
     rules: [
       {
@@ -35,7 +43,11 @@ let config = {
           options: {
             jsc: {
               transform: {
-                react: { runtime: 'automatic', refresh: isDev },
+                react: {
+                  runtime: 'automatic',
+                  importSource: '@emotion/react',
+                  refresh: isDev,
+                },
               },
             },
           },
@@ -51,10 +63,20 @@ let config = {
       },
     ],
   },
+
   plugins: [
     isAnalyze && new BundleAnalyzerPlugin(),
     isDev && new ReactRefreshWebpackPlugin(),
+    isProd && new MiniCssExtractPlugin({ filename: 'css/index.[contenthash:6].css' }),
+    isProd &&
+      new CopyPlugin({
+        patterns: [
+          { from: 'public', to: '.', filter: filePath => !filePath.endsWith('index.html') },
+        ],
+      }),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') }),
   ].filter(Boolean),
+
   devServer: {
     port: 3000,
     static: {
